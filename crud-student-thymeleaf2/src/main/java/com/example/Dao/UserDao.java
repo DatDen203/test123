da02cde01645625fcfaca840ba6514f5a0129b16
NgaLe02 <ngalee02@gmail.com>
2023-04-23 21:35:40 +0700

    config mybatis

--- crud-student-thymeleaf2/src/main/java/com/example/Dao/UserDao.java
+++ crud-student-thymeleaf2/src/main/java/com/example/Dao/UserDao.java
@@ -1,30 +1,50 @@
 package com.example.Dao;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.example.Model.UserDto;
 import com.example.Model.UserLogin;
+import com.example.util.MyBatisUtil;
 
 @Repository
 public class UserDao implements IUserDao {
 	@Autowired
 	JdbcTemplate _jdbcTemplate;
-
+	
+	public UserDao() {
+		super();
+	}
+    
+	
+//	Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
 	public int register(UserDto obj) {
 		String sql = "INSERT INTO user (name, username, email,pass) VALUES (?,?,?,?)";
 		return _jdbcTemplate.update(sql, obj.getName(), obj.getUsername(), obj.getEmail(), obj.getPass());
 	}
 	
 	public UserDto validateLogin(UserLogin obj) {
-		String sql = "select * from user where username='" + obj.getUsername() + "' and pass='" + obj.getPass() + "'";
-		List<UserDto> count = _jdbcTemplate.query(sql, new UserMapper());
-		return count.size() > 0 ? count.get(0) : null;
+//		String sql = "select * from user where username='" + obj.getUsername() + "' and pass='" + obj.getPass() + "'";
+//		List<UserDto> count = _jdbcTemplate.query(sql, new UserMapper());
+//		return count.size() > 0 ? count.get(0) : null;
+		
+		UserDto userDto = null;
+		
+		Map<String, String> paramMap = new HashMap<>();
+		paramMap.put("username", obj.getUsername());
+		paramMap.put("pass", obj.getPass());
+		
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		userDto = session.selectOne("selectUsernameAndPass", paramMap);
+		session.commit();
+		session.close();
+		return userDto;
 	}
 
 	public int update(UserDto obj) {
@@ -32,8 +52,14 @@ public class UserDao implements IUserDao {
 		return _jdbcTemplate.update(sql, obj.getName(), obj.getUsername(), obj.getEmail(), obj.getIdUser());
 	}
 
+	public UserDto getUserByUsername(String username) {
+		String sql = "select * from user where username=?";
+		return _jdbcTemplate.queryForObject(sql, new UserMapper(), new Object[] { username });
+	}
+
 	public String validateReigister(UserDto obj) {
-		int count = _jdbcTemplate.queryForObject("select count(*) from user where username=?", Integer.class, obj.getUsername());
+		int count = _jdbcTemplate.queryForObject("select count(*) from user where username=?", Integer.class,
+				obj.getUsername());
 		if (count == 1) {
 			return "Error username";
 		}
@@ -43,21 +69,35 @@ public class UserDao implements IUserDao {
 		}
 		return "true";
 	}
-
-	public int ChangePass(UserDto obj) {
-		String sql = "update user set pass=? where email = ?";
-		return _jdbcTemplate.update(sql, obj.getPass(), obj.getEmail());
-	}
-
-	public UserDto findUserByEmail(String email) {
-		try {
-			String sql = "select * from user where email = '"+email+"'";
-			return _jdbcTemplate.queryForObject(sql, new UserMapper());
-		} catch (EmptyResultDataAccessException e) {
-			return null;
-		} catch (IncorrectResultSizeDataAccessException e) {
-			return null;
-		}
+	public static void main(String[] args) throws IOException {
+//		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+//		//@SuppressWarnings("unchecked")
+//		List<UserDto> userDtoList = session.selectList("selectAll");
+//		session.commit();
+//		session.close();
+////		return userDtoList;
+//		System.out.println(userDtoList);
 		
+        UserDto userDto = null;
+		
+		Map<String, String> paramMap = new HashMap<>();
+		paramMap.put("username", "ngale");
+		paramMap.put("pass", "123");
+		
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		userDto = session.selectOne("selectUsernameAndPass", paramMap);
+		session.commit();
+		session.close();
+		System.out.println(userDto);
+    }
+
+
+	@Override
+	public UserDto findUserByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
 	}
+
+
+	
 }
