1f67dcd16a6e70e38939d52598e9f73c0bbf8e06
DatDen203 <tiendat2030415@gmail.com>
2023-05-24 09:58:00 +0700

    update logout

---
+++ crud-student-thymeleaf2/src/main/java/com/example/config/security.java
@@ -0,0 +1,59 @@
+package com.example.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class security extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username,password, enabled from users where username=?")
+				.authoritiesByUsernameQuery(
+						"Select username, concat('ROLE_',user_role) user_role from user_roles where username=?").passwordEncoder(passwordEncoder());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/list")
+			.access("hasRole('ROLE_ADMIN')")
+		    .anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.loginProcessingUrl("/doLogin")
+			.loginPage("/login").permitAll()
+			.passwordParameter("pass")
+			.usernameParameter("username")
+			.defaultSuccessUrl("/list")
+			.failureUrl("/login?error")
+			.and()
+			.logout()
+			.permitAll()
+			.logoutUrl("/logoutSuccess")
+			.logoutSuccessUrl("/login?logout=true")
+			.and()
+			.csrf().disable()
+			;
+	}
+}
